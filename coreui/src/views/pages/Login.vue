1dc9ea81fa956216a451601d00622f7b40326279
Gianfranco <Gianfrancor442@gmail.com>
2021-03-04 19:46:07 -0400

    Libreria Loading

--- coreui/src/views/pages/Login.vue
+++ coreui/src/views/pages/Login.vue
@@ -1,70 +1,84 @@
 <template>
   <div>
-    <loading-overlay :active="Loading" :is-full-page="true" loader="bars" />
-  <CContainer class="d-flex content-center min-vh-100">
-    <CRow>
-      <CCol>
-        <CCardGroup>
-          <CCard class="p-4">
-            <CCardBody>
-              <CForm @submit.prevent="login" method="POST">
-                <h1>Login</h1>
-                <p class="text-muted">Sign In to your account</p>
-                <CInput
-                  v-model="email"
-                  prependHtml="<i class='cui-user'></i>"
-                  placeholder="Username"
-                  autocomplete="username email"
-                >
-                  <template #prepend-content><CIcon name="cil-user"/></template>
-                </CInput>
-                <CInput
-                  v-model="password"
-                  prependHtml="<i class='cui-lock-locked'></i>"
-                  placeholder="Password"
-                  type="password"
-                  autocomplete="curent-password"
-                >
-                  <template #prepend-content
-                    ><CIcon name="cil-lock-locked"
-                  /></template>
-                </CInput>
-                <CRow>
-                  <CCol col="6">
-                    <CButton type="submit" color="primary" class="px-4"
-                      >Login</CButton
-                    >
-                  </CCol>
-                  <CCol col="6" class="text-right">
-                    <CButton color="link" class="px-0"
-                      >Forgot password?</CButton
-                    >
-                  </CCol>
-                </CRow>
-              </CForm>
-            </CCardBody>
-          </CCard>
-          <CCard
-            color="primary"
-            text-color="white"
-            class="text-center py-5 d-md-down-none"
-            body-wrapper
-          >
-            <h2>Sign up</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-            <CButton color="primary" class="active mt-3" @click="goRegister()">
-              Register Now!
-            </CButton>
-          </CCard>
-        </CCardGroup>
-      </CCol>
-    </CRow>
-  </CContainer>
-    </div>
-
+    <loading-overlay
+      :active="Loading"
+      :is-full-page="true"
+      loader="bars"
+      color="#052D6D"
+    />
+    <CContainer class="d-flex content-center min-vh-100">
+      <CRow>
+        <CCol>
+          <CCardGroup>
+            <CCard class="p-4">
+              <CCardBody>
+                <CForm @submit.prevent="login" method="POST">
+                  <h1>Login</h1>
+                  <p class="text-muted">Sign In to your account</p>
+                  <CInput
+                    v-model="email"
+                    prependHtml="<i class='cui-user'></i>"
+                    placeholder="Username"
+                    autocomplete="username email"
+                  >
+                    <template #prepend-content
+                      ><CIcon name="cil-user"
+                    /></template>
+                  </CInput>
+                  <CInput
+                    v-model="password"
+                    prependHtml="<i class='cui-lock-locked'></i>"
+                    placeholder="Password"
+                    type="password"
+                    autocomplete="curent-password"
+                  >
+                    <template #prepend-content
+                      ><CIcon name="cil-lock-locked"
+                    /></template>
+                  </CInput>
+                  <CRow>
+                    <CCol col="6">
+                      <CButton
+                        type="submit"
+                        @click.prevent="login"
+                        color="primary"
+                        class="px-4"
+                        >Login</CButton
+                      >
+                    </CCol>
+                    <CCol col="6" class="text-right">
+                      <CButton color="link" class="px-0"
+                        >Forgot password?</CButton
+                      >
+                    </CCol>
+                  </CRow>
+                </CForm>
+              </CCardBody>
+            </CCard>
+            <CCard
+              color="primary"
+              text-color="white"
+              class="text-center py-5 d-md-down-none"
+              body-wrapper
+            >
+              <h2>Sign up</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <CButton
+                color="primary"
+                class="active mt-3"
+                @click="goRegister()"
+              >
+                Register Now!
+              </CButton>
+            </CCard>
+          </CCardGroup>
+        </CCol>
+      </CRow>
+    </CContainer>
+  </div>
 </template>
 
 <script>
@@ -88,6 +102,7 @@ export default {
     login() {
       this.Loading = true;
       let self = this;
+      die();
       axios
         .post(this.$apiAdress + "/api/login", {
           email: self.email,
