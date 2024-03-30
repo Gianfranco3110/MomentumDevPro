c275e30cf94624c4ea24d7d45558948c69e0b31e
pedro.rodriguez4 <pedro.rodriguez@datia.co>
2024-03-29 20:20:55 -0400

    comentario de seeder stated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,7 +34,7 @@ class DatabaseSeeder extends Seeder
             CourseSectionSeeder::class,
             CourseSeeder::class,
             CourseVideoSeeder::class,
-            // StateSeeder::class
+            StateSeeder::class
         ]);
     }
 }
