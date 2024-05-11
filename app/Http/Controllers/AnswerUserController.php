f8b87bce90f61d97cfccf900ddd36d5f23385b25
Gianfranco <gianfrancor442@gmail.com>
2024-05-10 22:05:58 -0400

    answer user test

--- app/Http/Controllers/AnswerUserController.php
+++ app/Http/Controllers/AnswerUserController.php
@@ -12,6 +12,7 @@ class AnswerUserController extends Controller
     {
         // dd($request);
         if($request->input('tipo') === '1'){
+            return response($request);
             $data = $request->input('answer');
             $respDta = [];
             foreach ($data as $key=>$item) {
