91cccc1cd42684883d8508d46bbad273967b52fa
Gianfranco <gianfrancor442@gmail.com>
2024-05-13 21:03:18 -0400

    add-variable de qa

--- coreui/src/main.js
+++ coreui/src/main.js
@@ -20,7 +20,11 @@ export const EventBus = new Vue();
 
 Vue.prototype.$bus = new Vue();
 
+//Local
 Vue.prototype.$apiAdress = 'http://127.0.0.1:8000'
+
+//QA
+//Vue.prototype.$apiAdress = 'https://yariapi.furgoestacas.com'
 
 Vue.config.performance = true
 Vue.use(CoreuiVue)
