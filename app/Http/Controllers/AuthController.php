4b4e698b1c5c33d3882738691353239904494bf1
Gianfranco <gianfrancor442@gmail.com>
2024-08-03 07:03:52 -0400

    correccion de pais

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -68,7 +68,7 @@ class AuthController extends Controller
         $user->adress_all = $request->adress_all;
         $user->type_document = $request->type_document;
         $user->number_document = $request->number_document;
-        $user->country = $request->countries;
+        $user->country = $request->country;
         $user->stated = $request->stated;
         $user->municipality = $request->municipality;
         $user->street = $request->street;
